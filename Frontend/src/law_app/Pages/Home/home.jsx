73b4c50c438fe181edd6ed0158e61aadcd6161f0
magendraprasat <magendraprasatmg@gmail.com>
2024-10-02 18:18:57 +0530

    Updated google translate component

--- Frontend/src/law_app/Pages/Home/home.jsx
+++ Frontend/src/law_app/Pages/Home/home.jsx
@@ -1,62 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import GoogleTranslate from '../../components/GoogleTranslate';
 
 const Home = () => {
 
-  const [clickCount, setClickCount] = useState(0);
-
-  const googleTranslateElementInit = () => {
-
-    if (window.google && window.google.translate) {
-      const existingElement = document.getElementById('google_translate_element');
-      if (existingElement && !existingElement.querySelector('.skiptranslate')) {
-        new window.google.translate.TranslateElement(
-          {
-            pageLanguage: 'en',
-            autoDisplay: false,
-          },
-          'google_translate_element'
-        );
-      }
-    }
-  };
-
-  useEffect(() => {
-
-    const existingScript = document.getElementById('google-translate-script');
-    if (!existingScript) {
-      const addScript = document.createElement('script');
-      addScript.setAttribute('id', 'google-translate-script');
-      addScript.setAttribute(
-        'src',
-        '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
-      );
-      document.body.appendChild(addScript);
-      window.googleTranslateElementInit = googleTranslateElementInit;
-    }
-
-    return () => {
-      const script = document.getElementById('google-translate-script');
-      if (script) {
-        document.body.removeChild(script);
-        delete window.googleTranslateElementInit;
-      }
-    };
-  }, []);
 
   return (
 
     <>
-      <div onClick={() => {
-        setClickCount(prev => prev + 1)
-        if (clickCount % 1 === 0 && clickCount !== 0) {
-          window.location.reload();
-        }
-      }}>
-        <div id="google_translate_element"></div>
-      </div>
-      <h4>Start building your app. Happy Coding!</h4>
-      <GoogleTranslate />
+        <GoogleTranslate />
+
+        <h1>Home</h1>
+        <p>The check for the laws</p>
+        <p>This is to host the react build file</p>
+
     </>
   );
 };
--- Frontend/src/law_app/components/GoogleTranslate.jsx
+++ Frontend/src/law_app/components/GoogleTranslate.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 
 const GoogleTranslate = () => {
-  return (
-    <>
-        <div className='para'>
-            <p>Google Translate</p>
-            <p>
-                Hi hello
-            </p>
-            {/* write some sentence about nature */}
 
-            <p>
-                Nature is amazing 
-            </p>
-            <p>
-                Nature is amazing and beautiful 
-            </p>
-        </div>
-    </>
-  )
+
+    const [clickCount, setClickCount] = useState(0);
+
+    const googleTranslateElementInit = () => {
+
+        if (window.google && window.google.translate) {
+            const existingElement = document.getElementById('google_translate_element');
+            if (existingElement && !existingElement.querySelector('.skiptranslate')) {
+                new window.google.translate.TranslateElement(
+                    {
+                        pageLanguage: 'en',
+                        autoDisplay: false,
+                    },
+                    'google_translate_element'
+                );
+            }
+        }
+    };
+
+    useEffect(() => {
+
+        const existingScript = document.getElementById('google-translate-script');
+        if (!existingScript) {
+            const addScript = document.createElement('script');
+            addScript.setAttribute('id', 'google-translate-script');
+            addScript.setAttribute(
+                'src',
+                '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+            );
+            document.body.appendChild(addScript);
+            window.googleTranslateElementInit = googleTranslateElementInit;
+        }
+
+        return () => {
+            const script = document.getElementById('google-translate-script');
+            if (script) {
+                document.body.removeChild(script);
+                delete window.googleTranslateElementInit;
+            }
+        };
+    }, []);
+
+    return (
+        <>
+            <div onClick={() => {
+                setClickCount(prev => prev + 1)
+                if (clickCount % 1 === 0 && clickCount !== 0) {
+                    window.location.reload();
+                }
+            }}>
+                <div id="google_translate_element"></div>
+            </div>
+        </>
+    )
 }
 
 export default GoogleTranslate
